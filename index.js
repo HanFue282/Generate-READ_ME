3c095a91640923ddbc1c7968ae673afafa4bd63b
HanFue282 <59034654+HanFue282@users.noreply.github.com>
2020-02-14 01:31:46 -0600

    tested README.md and index.js files for results

--- index.js
+++ index.js
@@ -4,161 +4,177 @@ const util = require("util");
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
-const questions = [
-    "What's your name?",
-    "What's your favorite animal?",
-    "Where are you learning coding?",
-    "How much sleep do you get?"
-];
+getInfo();
 
-const api = {
-    getUser(username) {
+async function getInfo() {
+    try{
+        const { name } = await inquirer.prompt({
+            message: "What is your name?",
+            name: "name"
+        });
+        const { gitHub } = await inquirer.prompt({
+            message: "What's your GitHub username?",
+            name: "gitHub"
+        });
+        const { email } = await inquirer.prompt({
+            message: "What is your email for GitHub?",
+            name: "email"
+        });
+        const { title } = await inquirer.prompt({
+            message: "What would you like the title of your README.md?",
+            name: "title"
+        });
+
+        console.log(name, gitHub, email, title);
+
+    function readMeContent(){
+    return `
+# ${title}
+
+![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
+[![Website naereen.github.io](https://img.shields.io/website-up-down-green-red/https/naereen.github.io.svg)](https://naereen.github.io/)
+[![Ask Me Anything !](https://img.shields.io/badge/Ask%20me-anything-1abc9c.svg)](https://GitHub.com/Naereen/ama)
+            
+* __Description__: User are to generate a README.md file with using node.js. They are to retrieve the user's GitHub profile and their repository(single or all repository). Users will also be answering questions that will be answered below the Table of Contents.
   
+* __Table of Contents__:
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [License](#license)
+    * [Credits](#credits)
+    * [Contributing](#contributing)
+    * [Tests](#tests)
+    * [Questions](#questions)
+  
+  ## Installation
+  
+  What did you use to install your project?
+  
+  ## Usage
+  
+  (Provide photo of)
+  
+  ## License
+  
+  (Ask teacher about license)
+  
+  ## Credits
+  
+  Badges from [Nareen](https://github.com/Naereen/badges/blob/master/README.md)
+  
+  
+  ## Contributing
+  
+      Contributor Covenant Code of Conduct
+  
+      Our Pledge
+  
+      We as members, contributors, and leaders pledge to make participation in our community a harassment-free experience for everyone, regardless of age, body size, visible or invisible disability, ethnicity, sex characteristics, gender identity and expression, level of experience, education, socio-economic status, nationality, personal appearance, race, religion, or sexual identity and orientation.
+  
+      We pledge to act and interact in ways that contribute to an open, welcoming, diverse, inclusive, and healthy community.
+  
+       Our Standards
+  
+      Examples of behavior that contributes to a positive environment for our community include:
+  
+      * Demonstrating empathy and kindness toward other people
+      * Being respectful of differing opinions, viewpoints, and experiences
+      * Giving and gracefully accepting constructive feedback
+      * Accepting responsibility and apologizing to those affected by our mistakes, and learning from the experience
+      * Focusing on what is best not just for us as individuals, but for the overall community
+  
+      Examples of unacceptable behavior include:
+  
+      * The use of sexualized language or imagery, and sexual attention or
+      advances of any kind
+      * Trolling, insulting or derogatory comments, and personal or political attacks
+      * Public or private harassment
+      * Publishing others' private information, such as a physical or email
+      address, without their explicit permission
+      * Other conduct which could reasonably be considered inappropriate in a
+      professional setting
+  
+       Enforcement Responsibilities
+  
+      Community leaders are responsible for clarifying and enforcing our standards of acceptable behavior and will take appropriate and fair corrective action in response to any behavior that they deem inappropriate, threatening, offensive, or harmful.
+  
+      Community leaders have the right and responsibility to remove, edit, or reject comments, commits, code, wiki edits, issues, and other contributions that are not aligned to this Code of Conduct, and will communicate reasons for moderation decisions when appropriate.
+  
+       Scope
+  
+      This Code of Conduct applies within all community spaces, and also applies when an individual is officially representing the community in public spaces. Examples of representing our community include using an official e-mail address, posting via an official social media account, or acting as an appointed representative at an online or offline event.
+  
+       Enforcement
+  
+      Instances of abusive, harassing, or otherwise unacceptable behavior may be reported to the community leaders responsible for enforcement at [INSERT CONTACT METHOD]. All complaints will be reviewed and investigated promptly and fairly.
+  
+      All community leaders are obligated to respect the privacy and security of the reporter of any incident.
+  
+       Enforcement Guidelines
+  
+      Community leaders will follow these Community Impact Guidelines in determining the consequences for any action they deem in violation of this Code of Conduct:
+  
+      1. Correction
+  
+      **Community Impact**: Use of inappropriate language or other behavior deemed unprofessional or unwelcome in the community.
+  
+      **Consequence**: A private, written warning from community leaders, providing clarity around the nature of the violation and an explanation of why the behavior was inappropriate. A public apology may be requested.
+  
+      2. Warning
+  
+      **Community Impact**: A violation through a single incident or series of actions.
+  
+      **Consequence**: A warning with consequences for continued behavior. No interaction with the people involved, including unsolicited interaction with those enforcing the Code of Conduct, for a specified period of time. This includes avoiding interactions in community spaces as well as external channels like social media. Violating these terms may lead to a temporary or permanent ban.
+  
+      3. Temporary Ban
+  
+      **Community Impact**: A serious violation of community standards, including sustained inappropriate behavior.
+  
+      **Consequence**: A temporary ban from any sort of interaction or public communication with the community for a specified period of time. No public or private interaction with the people involved, including unsolicited interaction with those enforcing the Code of Conduct, is allowed during this period. Violating these terms may lead to a permanent ban.
+  
+      4. Permanent Ban
+  
+      **Community Impact**: Demonstrating a pattern of violation of community standards, including sustained inappropriate behavior,  harassment of an individual, or aggression toward or disparagement of classes of individuals.
+  
+      **Consequence**: A permanent ban from any sort of public interaction within the community.
+  
+       Attribution
+  
+      This Code of Conduct is adapted from the [Contributor Covenant][homepage], version 2.0,
+      available at https://www.contributor-covenant.org/version/2/0/code_of_conduct.html.
+  
+      Community Impact Guidelines were inspired by [Mozilla's code of conduct enforcement ladder](https://github.com/mozilla/diversity).
+  
+      [homepage]: https://www.contributor-covenant.org
+  
+      For answers to common questions about this code of conduct, see the FAQ at
+      https://www.contributor-covenant.org/faq. Translations are available at https://www.contributor-covenant.org/translations.
+  
+  
+  ## Tests
+  
+  - Having to first write down the README.md file in order for the results to appear.
+  - Second is to make the prompts/questions the user has to answer.
+  - Test running the prompts to see if they appear in console
+  - Now that the answers are being displayed in console. Have the README.md format into a fucntion so that the contents can have the answers linked into the questions below.
+  - Tested several times for having the README.md file name to be changed, but I only managed to have the title of it changes.
+  - File is generated into a README.md file along with the answers for the prompts that are displayed below.
+  
+  ## Questions
+
+  What is your name? ${name}
+  
+  What is your GitHub username? [${gitHub}](https://github.com/${gitHub})
+  
+  What is your email for your GitHub account? [${email}](https://mail.google.com/mail/u/0/#inbox?compose=new) `
     }
-  };
+    await writeFileAsync(
+         "README.md",
+        readMeContent()
+        );
 
-function writeToFile(fileName, data) {
-    if (err)
-    console.log(err);
-
-
+    } catch (err) {
+        console.log(err);
+    }
 }
 
-function readMeContent(){
-    return `# Generate-README.md 
-    ![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
-    [![Website naereen.github.io](https://img.shields.io/website-up-down-green-red/https/naereen.github.io.svg)](https://naereen.github.io/)
-    [![Ask Me Anything !](https://img.shields.io/badge/Ask%20me-anything-1abc9c.svg)](https://GitHub.com/Naereen/ama)
-    
-      * __Description__: User are to generate a README.md file with using node.js. They are to retrieve the user's GitHub profile and their repository(single or all repository). Users will also be answering questions that will be answered below the Table of Contents.
-    
-      * __Table of Contents__:
-        * [Installation](#installation)
-        * [Usage](#usage)
-        * [License](#license)
-        * [Credits](#credits)
-        * [Contributing](#contributing)
-        * [Tests](#tests)
-        * [Questions](#questions)
-    
-    ## Installation
-    
-    What did you use to install your project?
-    
-    ## Usage
-    
-    (Provide photo of)
-    
-    ## License
-    
-    (Ask teacher about license)
-    
-    ## Credits
-    
-    Badges from [Nareen](https://github.com/Naereen/badges/blob/master/README.md)
-    
-    
-    ## Contributing
-    
-        Contributor Covenant Code of Conduct
-    
-        Our Pledge
-    
-        We as members, contributors, and leaders pledge to make participation in our community a harassment-free experience for everyone, regardless of age, body size, visible or invisible disability, ethnicity, sex characteristics, gender identity and expression, level of experience, education, socio-economic status, nationality, personal appearance, race, religion, or sexual identity and orientation.
-    
-        We pledge to act and interact in ways that contribute to an open, welcoming, diverse, inclusive, and healthy community.
-    
-         Our Standards
-    
-        Examples of behavior that contributes to a positive environment for our community include:
-    
-        * Demonstrating empathy and kindness toward other people
-        * Being respectful of differing opinions, viewpoints, and experiences
-        * Giving and gracefully accepting constructive feedback
-        * Accepting responsibility and apologizing to those affected by our mistakes, and learning from the experience
-        * Focusing on what is best not just for us as individuals, but for the overall community
-    
-        Examples of unacceptable behavior include:
-    
-        * The use of sexualized language or imagery, and sexual attention or
-        advances of any kind
-        * Trolling, insulting or derogatory comments, and personal or political attacks
-        * Public or private harassment
-        * Publishing others' private information, such as a physical or email
-        address, without their explicit permission
-        * Other conduct which could reasonably be considered inappropriate in a
-        professional setting
-    
-         Enforcement Responsibilities
-    
-        Community leaders are responsible for clarifying and enforcing our standards of acceptable behavior and will take appropriate and fair corrective action in response to any behavior that they deem inappropriate, threatening, offensive, or harmful.
-    
-        Community leaders have the right and responsibility to remove, edit, or reject comments, commits, code, wiki edits, issues, and other contributions that are not aligned to this Code of Conduct, and will communicate reasons for moderation decisions when appropriate.
-    
-         Scope
-    
-        This Code of Conduct applies within all community spaces, and also applies when an individual is officially representing the community in public spaces. Examples of representing our community include using an official e-mail address, posting via an official social media account, or acting as an appointed representative at an online or offline event.
-    
-         Enforcement
-    
-        Instances of abusive, harassing, or otherwise unacceptable behavior may be reported to the community leaders responsible for enforcement at [INSERT CONTACT METHOD]. All complaints will be reviewed and investigated promptly and fairly.
-    
-        All community leaders are obligated to respect the privacy and security of the reporter of any incident.
-    
-         Enforcement Guidelines
-    
-        Community leaders will follow these Community Impact Guidelines in determining the consequences for any action they deem in violation of this Code of Conduct:
-    
-        1. Correction
-    
-        **Community Impact**: Use of inappropriate language or other behavior deemed unprofessional or unwelcome in the community.
-    
-        **Consequence**: A private, written warning from community leaders, providing clarity around the nature of the violation and an explanation of why the behavior was inappropriate. A public apology may be requested.
-    
-        2. Warning
-    
-        **Community Impact**: A violation through a single incident or series of actions.
-    
-        **Consequence**: A warning with consequences for continued behavior. No interaction with the people involved, including unsolicited interaction with those enforcing the Code of Conduct, for a specified period of time. This includes avoiding interactions in community spaces as well as external channels like social media. Violating these terms may lead to a temporary or permanent ban.
-    
-        3. Temporary Ban
-    
-        **Community Impact**: A serious violation of community standards, including sustained inappropriate behavior.
-    
-        **Consequence**: A temporary ban from any sort of interaction or public communication with the community for a specified period of time. No public or private interaction with the people involved, including unsolicited interaction with those enforcing the Code of Conduct, is allowed during this period. Violating these terms may lead to a permanent ban.
-    
-        4. Permanent Ban
-    
-        **Community Impact**: Demonstrating a pattern of violation of community standards, including sustained inappropriate behavior,  harassment of an individual, or aggression toward or disparagement of classes of individuals.
-    
-        **Consequence**: A permanent ban from any sort of public interaction within the community.
-    
-         Attribution
-    
-        This Code of Conduct is adapted from the [Contributor Covenant][homepage], version 2.0,
-        available at https://www.contributor-covenant.org/version/2/0/code_of_conduct.html.
-    
-        Community Impact Guidelines were inspired by [Mozilla's code of conduct enforcement ladder](https://github.com/mozilla/diversity).
-    
-        [homepage]: https://www.contributor-covenant.org
-    
-        For answers to common questions about this code of conduct, see the FAQ at
-        https://www.contributor-covenant.org/faq. Translations are available at https://www.contributor-covenant.org/translations.
-    
-    
-    ## Tests
-    
-    Having to first write down the README.md file in order for the results to appear.
-    
-    ## Questions
-    
-    What is your GitHub username? ${gitHub}
-    
-    What is your email for your GitHub account? ${email} `
-}
-
-function init() {
-
-}
-
-init();
