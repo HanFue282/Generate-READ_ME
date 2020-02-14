ffc6f72e96eb189fbe556a0b92d653070a20793d
HanFue282 <59034654+HanFue282@users.noreply.github.com>
2020-02-13 21:33:00 -0600

    saving point for editting

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Generate-READ_ME
+# Generate-README.md
+
+  * Description: User are to generate a readme.md file with using node.js. They are to retrieve 
+  * Table of Contents
+  * Installation
+  * Usage
+  * License
+  * Contributing
+  * Tests
+  * Questions
---
+++ index.js
@@ -0,0 +1,19 @@
+const questions = [
+    "What's your name?",
+    "What's your favorite animal?",
+    "Where are you learning coding?",
+    "How much sleep do you get?"
+];
+
+function writeToFile(fileName, data) {
+    if (err)
+    console.log(err);
+
+
+}
+
+function init() {
+
+}
+
+init();
